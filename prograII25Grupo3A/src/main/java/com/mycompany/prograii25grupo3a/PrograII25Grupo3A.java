bc6bbe443cc5e31694a6fe638a015b4c5f7c81ab
DELL <DELL@DESKTOP-52ICNGO>
2025-08-21 16:05:52 -0600

    nombre

---
+++ prograII25Grupo3A/src/main/java/com/mycompany/prograii25grupo3a/PrograII25Grupo3A.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prograii25grupo3a;
+
+/**
+ *
+ * @author DELL
+ */
+public class PrograII25Grupo3A {
+
+    public static void main(String[] args) {
+        System.out.println("Danniel Eliezer Gómez Soto 6691-22-3902 ");
+        System.out.println(" ");
+        System.out.println(" ");
+        
+        
+    }
+}
